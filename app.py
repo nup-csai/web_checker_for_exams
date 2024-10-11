fc9c3f25df28060289b26c5f2a944d07c63e5685
KonstBeliakov <konstbeliakov@gmail.com>
2024-10-11 16:16:41 +0300

    Added ability to run programs from git

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import sys
 import os
 from contextlib import redirect_stdout
+import pygit2
+import shutil
 
 
 app = Flask(__name__)
@@ -16,11 +18,7 @@ def check_answer(answer, right_answer):
     return answer.strip() == right_answer.strip()
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = request.get_json()
-    code = data.get('text')
-
+def check_solution(code):
     test_directory_name = 'tests'
 
     test_filenames = sorted([i for i in os.listdir(test_directory_name) if i.startswith('test')])
@@ -37,7 +35,8 @@ def submit():
         input_filename = f'{test_directory_name}/{test_filename}'
         output_filename = 'output.txt'
         error_on_current_test = False
-        with open(input_filename, 'r', encoding='utf-8') as file, open(output_filename, 'w', encoding='utf-8') as output_file:
+        with open(input_filename, 'r', encoding='utf-8') as file, open(output_filename, 'w',
+                                                                       encoding='utf-8') as output_file:
             original_stdin = sys.stdin
             sys.stdin = file
             try:
@@ -60,6 +59,31 @@ def submit():
             break
 
     return jsonify(message=test_message)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    code = data.get('text')
+
+    return check_solution(code)
+
+
+@app.route('/check_solution_from_github', methods=['POST'])
+def check_solution_from_github():
+    local_path = './cloned_repository'
+
+    if os.path.exists(local_path) and os.path.isdir(local_path):
+        shutil.rmtree(local_path)
+
+    repo_url = 'https://github.com/KonstBeliakov/test_repository.git'
+    local_path = './cloned_repository'
+    pygit2.clone_repository(repo_url, local_path)
+
+    with open(f'./cloned_repository/main.py', 'r', encoding='utf-8') as f:
+        code = f.read()
+
+    return check_solution(code)
 
 
 if __name__ == '__main__':

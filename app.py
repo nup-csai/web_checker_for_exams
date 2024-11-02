20b9af79139e9ec70d054d62c76a9568c5ab43cf
KonstBeliakov <konstbeliakov@gmail.com>
2024-11-02 23:57:21 +0300

    It is now possible to check multiple repositories at the same time

--- app.py
+++ app.py
@@ -38,83 +38,89 @@ def apply_requirements():
 @app.route('/check_solution_from_github')
 def check_solution_from_github():
     def generate():
-        local_path = './cloned_repository'
+        data = [[] for _ in range(len(settings.repositories))]
+        for repo_idx, repo_url in enumerate(settings.repositories):
+            local_path = './cloned_repository'
 
-        utils.delete_folder(local_path)
+            utils.delete_folder(local_path)
 
-        repo_url = settings.repositories[0]
-        local_path = './cloned_repository'
-        pygit2.clone_repository(repo_url, local_path)
+            local_path = './cloned_repository'
+            pygit2.clone_repository(repo_url, local_path)
 
-        image_name = 'image1'
-        container_name = 'container1'
+            image_name = 'image1'
+            container_name = 'container1'
 
-        dockerfile_path = "./cloned_repository/Dockerfile"
+            dockerfile_path = "./cloned_repository/Dockerfile"
 
-        files_checking_results = [[f"'{filename}'", 'NS'] for filename in settings.files_that_should_exist]
-        checking_results = [[f'Test {i + 1}', 'NS'] for i in range(len(settings.routes))]
+            data[repo_idx] = {'routes_checking': [[f"'{filename}'", 'NS'] for filename in settings.files_that_should_exist],
+                              'files_checking': [[f'Test {i + 1}', 'NS'] for i in range(len(settings.routes))]}
 
-        yield f"data: {json.dumps({'routes_checking': checking_results, 'files_checking': files_checking_results})}\n\n"
+            #files_checking_results = [[f"'{filename}'", 'NS'] for filename in settings.files_that_should_exist]
+            #checking_results = [[f'Test {i + 1}', 'NS'] for i in range(len(settings.routes))]
 
-        # Checking for presence/absence of different files in the repository
-        for i, filename in enumerate(settings.files_that_should_exist):
-            file_path = f'./cloned_repository/{filename}'
-            path = Path(file_path)
-            if path.is_file():
-                files_checking_results[i][1] = 'OK'
-            else:
-                files_checking_results[i][1] = "Doesn't exist"
+            yield f"data: {json.dumps({'data': data})}\n\n"
 
-        yield f"data: {json.dumps({'routes_checking': checking_results, 'files_checking': files_checking_results})}\n\n"
-
-        # If there is no Dockerfile program will try to run main.py from the cloned repository
-        if not Path(dockerfile_path).is_file():
-            print(f'Dockerfile is not found. Trying to run main.py from {local_path}/')
-            try:
-                with open(f'./cloned_repository/main.py', 'r', encoding='utf-8') as f:
-                    code = f.read()
-                t1 = threading.Thread(target=lambda: exec(code))
-                t1.start()
-            except:
-                pass
-        else:
-            print('Dockerfile found')
-            utils.build_docker_container(dockerfile_path='./cloned_repository/', image_name=image_name,
-                                         container_name=container_name)
-
-        time.sleep(5)  # waiting for program to start
-
-        for i, (route, right_answer) in enumerate(settings.routes):
-            print(f'Test {i + 1}')
-            try:
-                url = f"http://localhost:8080{route}"
-                response = requests.get(url)
-                content = response.text
-                print(f'\troute: {route}')
-                print(f'\tcontent: {content}')
-                print(f'\tright answer: {right_answer}')
-            except Exception as e:
-                print(e)
-                checking_results.append([f'Test {i + 1}', 'RT'])
-            else:
-                if check_page_content(str(content), right_answer):
-                    checking_results[i][1] = 'OK'
+            # Checking for presence/absence of different files in the repository
+            for i, filename in enumerate(settings.files_that_should_exist):
+                file_path = f'./cloned_repository/{filename}'
+                path = Path(file_path)
+                if path.is_file():
+                    data[repo_idx]['routes_checking'][i][1] = 'OK'
                 else:
-                    checking_results[i][1] = 'WA'
-            yield f"data: {json.dumps({'routes_checking': checking_results, 'files_checking': files_checking_results})}\n\n"
+                    data[repo_idx]['routes_checking'][i][1] = "Doesn't exist"
 
-        print('Checking results:')
-        print(checking_results)
-        print()
+            #yield f"data: {json.dumps({'routes_checking': checking_results, 'files_checking': files_checking_results})}\n\n"
+            yield f"data: {json.dumps({'data': data})}\n\n"
 
-        print('Deleting a container...')
-        container = utils.client.containers.get(container_name)
-        container.stop()
-        container.remove()
+            # If there is no Dockerfile program will try to run main.py from the cloned repository
+            if not Path(dockerfile_path).is_file():
+                print(f'Dockerfile is not found. Trying to run main.py from {local_path}/')
+                try:
+                    with open(f'./cloned_repository/main.py', 'r', encoding='utf-8') as f:
+                        code = f.read()
+                    t1 = threading.Thread(target=lambda: exec(code))
+                    t1.start()
+                except:
+                    pass
+            else:
+                print('Dockerfile found')
+                utils.build_docker_container(dockerfile_path='./cloned_repository/', image_name=image_name,
+                                             container_name=container_name)
 
-        print('Deleting an image...')
-        image = utils.client.images.get(image_name)
-        utils.client.images.remove(image.id)
+            time.sleep(5)  # waiting for program to start
+
+            for i, (route, right_answer) in enumerate(settings.routes):
+                print(f'Test {i + 1}')
+                try:
+                    url = f"http://localhost:8080{route}"
+                    response = requests.get(url)
+                    content = response.text
+                    print(f'\troute: {route}')
+                    print(f'\tcontent: {content}')
+                    print(f'\tright answer: {right_answer}')
+                except Exception as e:
+                    print(e)
+                    # checking_results.append([f'Test {i + 1}', 'RT'])
+                else:
+                    if check_page_content(str(content), right_answer):
+                        data[repo_idx]['files_checking'][i][1] = 'OK'
+                    else:
+                        data[repo_idx]['files_checking'][i][1] = 'WA'
+                #yield f"data: {json.dumps({'routes_checking': checking_results, 'files_checking': files_checking_results})}\n\n"
+                yield f"data: {json.dumps({'data': data})}\n\n"
+
+            print('Checking results:')
+            print(data[repo_idx]['files_checking'])
+            print()
+
+            print('Deleting a container...')
+            container = utils.client.containers.get(container_name)
+            container.stop()
+            container.remove()
+
+            print('Deleting an image...')
+            image = utils.client.images.get(image_name)
+            utils.client.images.remove(image.id)
 
     return Response(generate(), mimetype='text/event-stream')
 
--- static/js/main.js
+++ static/js/main.js
@@ -5,71 +5,84 @@ function add_table(data){
 
     document.getElementById('tableContainer').innerHTML = '';
 
-    const table = document.createElement('table');
-    const headerRow = document.createElement('tr');
-    const headers = ['Test №', 'Status'];
-    headers.forEach(headerText => {
-        const th = document.createElement('th');
-        th.textContent = headerText;
-        headerRow.appendChild(th);
-    });
-    table.appendChild(headerRow);
+    for (let i = 0;i < data.length;i ++) {
+        const h3 = document.createElement('h3');
+        h3.textContent = `Repository ${i+1}`
+        tableContainer.appendChild(h3);
+        const repo_data = data[i];
+        console.log(`repo_data: ${repo_data}`);
+        if (!repo_data.routes_checking){
+            const p = document.createElement('p');
+            p.textContent = 'Has not been checked yet.';
+            tableContainer.appendChild(p);
+            break;
+        }
+        const table = document.createElement('table');
+        const headerRow = document.createElement('tr');
+        const headers = ['Test №', 'Status'];
+        headers.forEach(headerText => {
+            const th = document.createElement('th');
+            th.textContent = headerText;
+            headerRow.appendChild(th);
+        });
+        table.appendChild(headerRow);
 
-    for (let i = 0; i < data.routes_checking.length; i++) {
-        const row = document.createElement('tr');
-        if (data.routes_checking[i][1] === 'OK') {
-            row.classList.add('light-green');
-        } else {
-            row.classList.add('light-red');
+        for (let i = 0; i < repo_data.routes_checking.length; i++) {
+            const row = document.createElement('tr');
+            if (repo_data.routes_checking[i][1] === 'OK') {
+                row.classList.add('light-green');
+            } else {
+                row.classList.add('light-red');
+            }
+
+            for (let j = 0; j < 2; j++) {
+                const td = document.createElement('td');
+                td.textContent = `${repo_data.routes_checking[i][j]}`;
+                row.appendChild(td);
+            }
+
+            table.appendChild(row);
         }
 
-        for (let j = 0; j < 2; j++) {
-            const td = document.createElement('td');
-            td.textContent = `${data.routes_checking[i][j]}`;
-            row.appendChild(td);
+        const routes_header = document.createElement("h4");
+        routes_header.textContent = "Routes";
+
+        const files_header = document.createElement("h4");
+        files_header.textContent = "Files";
+
+        tableContainer.appendChild(routes_header);
+        tableContainer.appendChild(table);
+        tableContainer.appendChild(files_header);
+
+        const table2 = document.createElement('table');
+        const headerRow2 = document.createElement('tr');
+        const headers2 = ['File', 'Status'];
+        headers2.forEach(headerText2 => {
+            const th = document.createElement('th');
+            th.textContent = headerText2;
+            headerRow2.appendChild(th);
+        });
+        table2.appendChild(headerRow2);
+
+        for (let i = 0; i < repo_data.files_checking.length; i++) {
+            const row = document.createElement('tr');
+            if (repo_data.files_checking[i][1] === 'OK') {
+                row.classList.add('light-green');
+            } else {
+                row.classList.add('light-red');
+            }
+
+            for (let j = 0; j < 2; j++) {
+                const td = document.createElement('td');
+                td.textContent = `${repo_data.files_checking[i][j]}`;
+                row.appendChild(td);
+            }
+
+            table2.appendChild(row);
         }
 
-        table.appendChild(row);
+        tableContainer.appendChild(table2);
     }
-
-    const routes_header = document.createElement("h3");
-    routes_header.textContent = "Routes";
-
-    const files_header = document.createElement("h3");
-    files_header.textContent = "Files";
-
-    tableContainer.appendChild(routes_header);
-    tableContainer.appendChild(table);
-    tableContainer.appendChild(files_header);
-
-    const table2 = document.createElement('table');
-    const headerRow2 = document.createElement('tr');
-    const headers2 = ['File', 'Status'];
-    headers2.forEach(headerText2 => {
-        const th = document.createElement('th');
-        th.textContent = headerText2;
-        headerRow2.appendChild(th);
-    });
-    table2.appendChild(headerRow2);
-
-    for (let i = 0; i < data.files_checking.length; i++) {
-        const row = document.createElement('tr');
-        if (data.files_checking[i][1] === 'OK') {
-            row.classList.add('light-green');
-        } else {
-            row.classList.add('light-red');
-        }
-
-        for (let j = 0; j < 2; j++) {
-            const td = document.createElement('td');
-            td.textContent = `${data.files_checking[i][j]}`;
-            row.appendChild(td);
-        }
-
-        table2.appendChild(row);
-    }
-
-    tableContainer.appendChild(table2);
 }
 
 
@@ -79,7 +92,7 @@ document.getElementById('check_solutions').onclick = function () {
     eventSource.onmessage = function(event) {
         try {
             const data = JSON.parse(event.data);
-            add_table(data);
+            add_table(data.data);
         } catch (error) {
             console.log('error');
             console.log(event.data);
